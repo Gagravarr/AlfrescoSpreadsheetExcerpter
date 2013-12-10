c9cffa6ef1c4527f1eb10557c48ccd3b74f9e5e4
Nick Burch <nick@gagravarr.org>
2013-12-10 10:41:15 +0000

    Stub out the action tests

---
+++ src/test/java/com/quanticate/opensource/spreadsheetexcerpt/action/TestSpreadsheetExcerptAction.java
@@ -0,0 +1,40 @@
+package com.quanticate.opensource.spreadsheetexcerpt.action;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestSpreadsheetExcerptAction
+{
+   private SpreadsheetExcerptActionExecutor action;
+   
+   @BeforeClass
+   public static void setup()
+   {
+      // TODO Pull up a context
+      // TODO Get the action executor
+   }
+   
+   @Test
+   public void invalidParameters() throws Exception
+   {
+      // TODO
+   }
+   
+   @Test
+   public void basicExcerptXLS() throws Exception
+   {
+      // TODO
+   }
+   
+   @Test
+   public void basicExcerptXLSX() throws Exception
+   {
+      // TODO
+   }
+   
+   @Test
+   public void allParameters() throws Exception
+   {
+      // TODO
+   }
+}
